b07a4eb362e49608d924e8826e8e817535880cd8
abdul <abdul.tanveer@gmail.com>
2024-09-09 15:21:45 +0530

    onclick handler

--- app/src/main/java/com/example/pyplapp/MainActivity.kt
+++ app/src/main/java/com/example/pyplapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pyplapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,12 +10,19 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.pyplapp.kotlinexs.Employee
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)//layout inflation
+        mainTextView = findViewById(R.id.tvMain)
 
         var myEmmplyee = Employee("ename")
 
+    }
+
+    fun clickHandler(view: View) {
+        mainTextView.setText("pypl")
     }
 }
